c36ce8371eb7fb23ef5262faa3c5853aa34b8dba
Vovan-Obivan <podlesnih.vova@yandex.ru>
2024-12-10 21:21:25 +0400

    Update 7_1.py

--- 7_1.py
+++ 7_1.py
@@ -12,37 +12,38 @@ class Product:
 
 
 class Shop:
-    def __init__(self):
-        self._file_name = 'product.txt'
+    __file_name = 'products.txt'
 
     def get_products(self):
-        self._file_name = 'product.txt'
-        file = open(self._file_name, 'r')
-        products = file.read()
-        file.close()
-
-        return products
+        try:
+            with open(self.__file_name, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            return 'Ошибка «Файл не найден».'
 
     def add(self, *products):
-        for product in products:
-            if product.name not in self.get_products():
-                file = open(self._file_name, 'a')
-                file.write(product.__str__() + '\n')
-                
-            else:
-                print(f'Продукт {product.name} уже есть в магазине')
+        existing_products = self.get_products().split('\n')
+        existing_product_names = {line.split(', ')[0] for line in existing_products if line}
+        with open(self.__file_name, 'a') as file:
+            for product in products:
+                if product.name in existing_product_names:
+                    print(f'Продукт {product.name} уже есть в магазине')
+                else:
+                    file.write(str(product) + '\n')
+                    existing_product_names.add(product.name)
 
 
 s1 = Shop()
-p1 = Product('Potato',  50.5, 'Vegitables')
+p1 = Product('Potato', 50.5, 'Vegitables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 s1.add(p1, p2, p3)
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.get_products()
 
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
